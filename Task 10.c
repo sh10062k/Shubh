395e416dd3839026e1e8b6997953baa1a0a9342b
sh10062k <54353821+sh10062k@users.noreply.github.com>
2019-09-03 15:31:14 +0530

    Add files via upload

--- Task 10.c
+++ Task 10.c
@@ -1,16 +1,36 @@
 #include<stdio.h>
 #include<conio.h>
 
+//Declaring prototypes
+void Getdata();
+void Display();
+
+struct BOOK //Creating structure named book
+{
+	char Title[10], Author[10], Genre[10];
+};
+
+struct BOOK b; //Declaring variable of structure type
+
 void main()
 {
-	int i,j;
-	
-	for(i=5;i>0;i--)
-	{
-		for(j=1;j<=i;j++)
-		{
-			printf("%d",j);
-		}
-		printf("\n");
-	}
+	//Calling the functions
+	Getdata();
+	Display();
+}
+
+void Getdata()
+{
+	//Reading the title of the book, its author and its genre
+	printf("Enter the title of book: ");
+	gets(b.Title);
+	printf("Enter the name of author of the book: ");
+	gets(b.Author);
+	printf("Enter the genre of the book: ");
+	gets(b.Genre);	
+}
+void Display()
+{
+	//Displaying the output
+	printf("\nAccording to the given data,\nThe title of the book is: %s\nThe author's name is: %s\nThe genre of the book is: %s", b.Title, b.Author, b.Genre);
 }
